47ffcd5dde88af5e3abe828ca8ccf8799e68bb56
Sure-sh554 <sureshrathod0113@gmail.com>
2024-07-04 20:07:10 +0530

    Employee_Service

--- Employee-Servie/src/main/java/com/microservice/Employee/Servie/exception/GlobalExceptionHandler.java
+++ Employee-Servie/src/main/java/com/microservice/Employee/Servie/exception/GlobalExceptionHandler.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException,
-                                                                        WebRequest webRequest ){
-        ErrorDetails errorDetails=new ErrorDetails(
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
                 LocalDateTime.now(),
                 resourceNotFoundException.getMessage(),
                 webRequest.getDescription(false),
@@ -23,13 +24,25 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorDetails> handleEmailAlreadyExistsException(EmailAlreadyExistsException emailAlreadyExistsException,
-                                                                        WebRequest webRequest ){
-        ErrorDetails errorDetails=new ErrorDetails(
+                                                                          WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
                 LocalDateTime.now(),
                 emailAlreadyExistsException.getMessage(),
                 webRequest.getDescription(false),
                 "EMPLOYEE_EMAIL_ALREADY_EXISTS"
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleException(Exception exception,
+                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL_SERVER_ERROR"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
